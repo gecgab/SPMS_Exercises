f3f801d9e5a6d6f7abb8352a55b9da3f34c7499d
gecgab <56508814+gecgab@users.noreply.github.com>
2020-05-09 23:53:52 +0200

    Haversine formula + documentation comments added

--- Assignment01/assignment01.py
+++ Assignment01/assignment01.py
@@ -48,8 +48,6 @@ def compute_noise(param):
 # Don't modify this!
 ############################################
 RADIANT_TO_KM_CONSTANT = 6371.0088
-LATITUDE_TO_M_CONSTANT = 111000
-LONGITUDE_AT_EQUATOR_IN_M_CONSTANT = 111321
 epsilon = 1.6/0.05
 
 #########################################################
@@ -81,11 +79,22 @@ print("data loaded")
 #      Please, solve this task here and document       #
 #       your code                                      #
 ########################################################
+
+# calculate distance between two points on a sphere
+# this can also be achieved with geopy.distance()
+
 def get_distance_in_meters(lat1, lon1, lat2, lon2):
-    lat_dist_in_m = (lat2-lat1)*LATITUDE_TO_M_CONSTANT
-    lon_dist_at_lat = LONGITUDE_AT_EQUATOR_IN_M_CONSTANT * (math.cos(rad_of_deg(lat1)) + math.cos(rad_of_deg(lat2))) / 2 # using average. should be ok since only looking at short distances
-    lon_dist_in_m = (lon2-lon1) * lon_dist_at_lat
-    distance = round(np.sqrt(lon_dist_in_m**2 + lat_dist_in_m**2)) # using pythagoras
+    rlat1, rlon1, rlat2, rlon2 = map(math.radians, [lat1, lon1, lat2, lon2])
+
+    dlon = rlon2 - rlon1
+    dlat = rlat2 - rlat1
+
+    # using Haversine fomula
+    a = math.sin(dlat / 2) ** 2 + math.cos(rlat1) * math.cos(rlat2) * math.sin(dlon / 2) ** 2
+    c = 2 * math.asin(math.sqrt(a))
+    # Earth's radius is RADIANT_TO_KM_CONSTANT = 6371 km
+    distance = RADIANT_TO_KM_CONSTANT * c * 1000
+
     return distance
 
 #########################################################
@@ -94,11 +103,18 @@ def get_distance_in_meters(lat1, lon1, lat2, lon2):
 #   #  Please, solve this task here and document        #
 #       your code                                       #
 ##########################################################
+
+# cycle through all POIs to find the one with shortest distance to user location
+
 def find_poi_with_min_dist(lat, lon):
+    # initializing variables with unlikely values which are supposed to be overridden during loop
     min_dist = 1000000
     poi_id = -1
+
     for i in range(len(poi_data[:,0])):
         dist = get_distance_in_meters(lat,lon,poi_data[i,2],poi_data[i,3])
+
+        # update min_dist if shorter distance is found (POI closer to user location)
         if dist < min_dist:
             min_dist = dist
             poi_id = poi_data[i,0]
@@ -106,10 +122,10 @@ def find_poi_with_min_dist(lat, lon):
 
 user_at_poi = [] # list with POIs by user ID
 
+print("starting to match users with POIs")
 for i in range(len(taxi_data)):
     poi_id = find_poi_with_min_dist(taxi_data[i, 4], taxi_data[i, 3])
     user_at_poi.append(poi_id)
-
 
 #########################################################
 #                                                       #
@@ -137,26 +153,33 @@ for user in range(len(taxi_data)):
 #  Please, solve these two tasks here and document      #
 #  your code                                            #
 ##########################################################
+## Privacy gain
+# Idea: find POIs with closest distance to obfuscated user locations and compare with original user-to-POI matching
+
 noisy_user_at_poi = []
 
 for i in range(len(taxi_data)):
     noisy_poi_id = find_poi_with_min_dist(noisy_latitude[i], noisy_longitude[i])
     noisy_user_at_poi.append(noisy_poi_id)
-correctly_identified = len(set(user_at_poi).intersection(set(noisy_user_at_poi)))   # number of correctly identified user locations
+
+correctly_identified = len(set(user_at_poi).intersection(set(noisy_user_at_poi))) # number of correctly identified user locations
 privacy_gain = 1 - correctly_identified/len(user_at_poi)
 print(f'privacy gain: {int(100*privacy_gain)}%')
 
 ## Utility loss
 # Idea: calculate distance to POI location from obfuscated (noisy) location
-# and subtract distance to POI from original location
+# and subtract distance to POI from original location.
+# There is a possibility that a user's location will be closer to the original POI.
+diff_distance = []
 
 total_extra_dist = 0
 for i in range(len(taxi_data)):
     poi_id = user_at_poi[i]
     obfuscated_distance_to_poi = get_distance_in_meters(noisy_latitude[i], noisy_longitude[i], poi_data[poi_id, 2], poi_data[poi_id, 3])
     actual_distance_to_poi = get_distance_in_meters(taxi_data[i, 4], taxi_data[i, 3], poi_data[poi_id, 2], poi_data[poi_id, 3])
-    diff = obfuscated_distance_to_poi - actual_distance_to_poi
-    total_extra_dist += diff # there is a possibility that a user's location will be closer to the POI. In that case diff is negative; the user will have to walk less.
+    diff = obfuscated_distance_to_poi - actual_distance_to_poi # diff can be negative. That means that the user will have to walk less.
+    total_extra_dist += diff
+    diff_distance.append(diff)
 
 utility_loss = total_extra_dist/len(taxi_data)
 print(f'utility loss: {int(utility_loss)}m additional walking distance')
